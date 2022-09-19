44f5f5adc8a617a0115e2ee50b63b5a01159af12
Stephen Hawes <sphawes@gmail.com>
2022-09-19 12:53:46 -0400

    updates define.h to have pins correct for rev07

--- code/firmware/src/define.h
+++ code/firmware/src/define.h
@@ -1,24 +1,26 @@
 #define DE   PA12
 #define _RE  PA11
 
-#define LED1 PA8   //yellow, on bottom of front face
-#define LED2 PB1   //green, on top of front face
-#define LED3 PB_2  //green, first on top face
-#define LED4 PB3  //green, second on top face
-#define LED5 PB4   //green, third on top face
+#define LED1 PA7   
+#define LED2 PA6
+#define LED3 PA4
+#define LED4 PA3
+#define LED5 PA2   
+#define LED6 PA1
+#define LED7 PA0
 
-#define SW1 PA3
-#define SW2 PA2
+#define SW1 PB2
+#define SW2 PB1
 
-#define DRIVE1   PB5
-#define DRIVE2   PB6
-#define PEEL1    PB7
-#define PEEL2    PA7
+#define DRIVE1   PB4
+#define DRIVE2   PB5
+#define PEEL1    PB3
+#define PEEL2    PA15
 
-#define ONE_WIRE PA0
+#define ONE_WIRE PA8
 
-#define DRIVE_ENC_A PA4
-#define DRIVE_ENC_B PA15
+#define DRIVE_ENC_A PB6
+#define DRIVE_ENC_B PB7
 
 //#define FILM_TENSION  PA3
 
